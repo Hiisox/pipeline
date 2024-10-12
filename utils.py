4ee2404ca6923098297368fe1413105cd8a8487e
hiisox <mehdmhaya@gmail.com>
2024-10-12 05:14:44 +0200

    download fonction

--- utils.py
+++
@@ -1,9 +0,0 @@
-def download_config():
-    i = 0
-    try :
-      file = open("pipeline/config.yaml","r")
-      file.close()
-    except FileNotFoundError:
-      i = 1
-    if i == 1:
-      !git clone https://github.com/Hiisox/pipeline.git
